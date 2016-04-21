43683110f798d71db43494e6ac7ab91fea1c7e16
jy_dingsufu <1099288529@qq.com>
2016-04-21 17:17:02 +0800

    仿微信页面滑动

---
+++ src/com/example/weixinchatview/Fragment_center.java
@@ -0,0 +1,19 @@
+package com.example.weixinchatview;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment_center extends Fragment{
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_center, container, false);
+		
+	    return view;
+	}
+	
+}
